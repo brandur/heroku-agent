c84a55abee3401c60ffd3a445a1248a9981de633
Brandur <brandur@mutelight.org>
2014-09-26 01:08:10 -0700

    Collapse signal.go back into server.go

--- server.go
+++ server.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 	"net/rpc"
 	"os"
+	"os/signal"
 	"syscall"
 	"time"
 )
@@ -37,7 +38,7 @@ func Serve() {
 	go http.Serve(controlListener, nil)
 
 	// handle common process-killing signals so we can gracefully shut down
-	go HandleSignals(proxyListener, controlListener)
+	go handleSignals(proxyListener, controlListener)
 
 	// periodically reap the cache and second factor store so that we don't
 	// bloat out of control
@@ -57,6 +58,21 @@ func Serve() {
 	if err != nil {
 		fail(err)
 	}
+}
+
+func handleSignals(listeners ...net.Listener) {
+	sigc := make(chan os.Signal, 1)
+	// wait for SIGINT, SIGKILL, or SIGTERM
+	signal.Notify(sigc, os.Interrupt, os.Kill, syscall.SIGTERM)
+
+	sig := <-sigc
+	logger.Printf("Caught signal %s: shutting down\n", sig)
+
+	// stop listening (and unlink the socket if unix type)
+	for _, listener := range listeners {
+		listener.Close()
+	}
+	os.Exit(0)
 }
 
 func initListener(socketPath string) net.Listener {
